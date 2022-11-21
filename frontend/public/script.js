21c2fca252fc59046a100ce472b98398e398d24b
szabiv96 <szabi.v96@gmail.com>
2022-11-21 14:14:46 +0100

    v6666

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -13,7 +13,17 @@ const imageComponent = (url, title, uploadDate, phName) => `
         <h2>${phName}</h2>
         <img src = "public/images/${url}" />
     </div>
-    `
+    `;
+
+
+const formComponent = () => `
+    <form>
+        <input type="file" name="file" />
+        <input type="text" name="title" />
+        <input type="text" name="phname" />
+        <button>Send!</button>
+    </form>
+`
 
 // kikonzololja a fetchImages mappát az index.html-re
 const init = async () => {
@@ -27,6 +37,20 @@ const init = async () => {
             "beforeend",
             imageComponent(image.url, image.title, image.uploadDate, image.phName));
     });
+
+    rootElement.insertAdjacentHTML("beforeend", formComponent());
+    const formElement = document.querySelector("form");
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        const formData = new FormData()
+        //appenddel csatolunk hozzá a json-hoz dolgokat vagy mi
+        formData.append("image", formElement.querySelector('input(name="file")').files[0])
+        formData.append("title", formElement.querySelector('input(name="title")').value)
+        formData.append("phname", formElement.querySelector('input(name="phname")').value)
+    });
 };
 
 init();
+
+//1:50:16
