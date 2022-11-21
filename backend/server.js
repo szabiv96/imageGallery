85c1af6cf4a8f04a9aba06eb3813c5a77ac4893c
szabiv96 <szabi.v96@gmail.com>
2022-11-21 13:57:10 +0100

    v3

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,25 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req,res) => {
+// alap elérési útvonal
+app.get("/", (req, res) => {
     res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
-app.use("/public", express.static(path.join(`${__dirname}/../frontend/public`)))
+// csinálunk egy elérést, hogy a következő funkcióban el tudjuk érni a képeket
+app.use("/public", express.static(path.join(`${__dirname}/../frontend/public`)));
 
+// újabb elérési útvonal
+app.get("/images", (req, res) => {
+    fs.readFile("images.json", (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        } else {
+            return res.json(JSON.parse(data));
+        }
+    });
+});
+
+// megadjuk a port számát amin elérhetjük a szerverünket
 app.listen(9000, (_) => console.log("http://127.0.0.1:9000"));
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,32 @@
 console.log("hy");
+
+//feccseled a kép mappát és aszem json filet csinál belőle vagy mi
+const fetchImages = async () => {
+    return fetch("/images")
+    .then(data => data.json())
+}
+
+const imageComponent = (url, title, uploadDate, phName) => `
+    <div class ="card">
+        <h2>${title}</h2>
+        <h2>${uploadDate}</h2>
+        <h2>${phName}</h2>
+        <img src = "public/images/${url}" />
+    </div>
+    `
+
+// kikonzololja a fetchImages mappát az index.html-re
+const init = async () => {
+    const data = await fetchImages();
+    console.log(data);
+
+    const rootElement = document.querySelector("#root");
+
+    data.map(image => {
+        rootElement.insertAdjacentHTML(
+            "beforeend",
+            imageComponent(image.url, image.title, image.uploadDate, image.phName));
+    });
+};
+
+init();
